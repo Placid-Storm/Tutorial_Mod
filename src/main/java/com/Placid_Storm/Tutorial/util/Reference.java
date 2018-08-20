717cf1a232914bea374e4327dc1ecf2fcc98e36a
Placid_Storm <cain.austin@gmail.com>
2018-08-20 10:59:36 -0500

    I forgot the armor recipe. Oh well, I fixed it now.

--- src/main/java/com/Placid_Storm/Tutorial/util/Reference.java
+++ src/main/java/com/Placid_Storm/Tutorial/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "pstm";
     public static final String NAME = "Tutorial Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.01";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
     public static final String CLIENT_PROXY_CLASS = "com.Placid_Storm.Tutorial.proxy.ClientProxy";
